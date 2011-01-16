60fb01de6eeb8799adad3861ccecc348a0714306
lothar <lothar>
2011-01-16 22:13:59 +0000

    Modificationy by hand to be corrected in UML or XSLT template before next translation.

--- interfaces/IApplicationBus.h
+++ interfaces/IApplicationBus.h
@@ -6,13 +6,13 @@ class lb_I_ApplicationBus :
 public lb_I_Unknown {
 public:
       
-	virtual void LB_STDCALL AnounceUser(PCHAR name, PCHAR password) = 0;
+	virtual void LB_STDCALL AnounceUser(char* name, char* password) = 0;
       
-	virtual void LB_STDCALL Echo(PCHAR& text) = 0;
+	virtual void LB_STDCALL Echo(char* text) = 0;
       
-	virtual void LB_STDCALL getServices(PCHAR& services) = 0;
+	virtual void LB_STDCALL getServices(char* services) = 0;
       
-	virtual void LB_STDCALL getServiceForProtocol(PCHAR protocol, PCHAR& service) = 0;
+	virtual void LB_STDCALL getServiceForProtocol(char* protocol, char* service) = 0;
       
 };
 
@@ -23,13 +23,13 @@ class lb_I_ApplicationBus_ProtocolTarget :
 public lb_I_ProtocolTarget {
 public:
       
-	virtual void LB_STDCALL AnounceUser(PCHAR name, PCHAR password) = 0;
+	virtual void LB_STDCALL AnounceUser(char* name, char* password) = 0;
       
-	virtual void LB_STDCALL Echo(PCHAR& text) = 0;
+	virtual void LB_STDCALL Echo(char* text) = 0;
       
-	virtual void LB_STDCALL getServices(PCHAR& services) = 0;
+	virtual void LB_STDCALL getServices(char* services) = 0;
       
-	virtual void LB_STDCALL getServiceForProtocol(PCHAR protocol, PCHAR& service) = 0;
+	virtual void LB_STDCALL getServiceForProtocol(char* protocol, char* service) = 0;
       
 };
 		
