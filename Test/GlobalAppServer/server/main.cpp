1678ab631101171f3ccc81079202392c147cf523
lothar <lothar>
2011-07-10 10:27:10 +0000

    Using correct break handler registration for Windows.

--- Test/GlobalAppServer/server/main.cpp
+++ Test/GlobalAppServer/server/main.cpp
@@ -4,32 +4,98 @@
 #include <conio.h>
 #include <signal.h>
 
+
 void handler(int sig) {
 	COUT << "Oops..." << ENDL;
+	UAP_REQUEST(getModuleInstance(), lb_I_PluginManager, PM)
+	PM->unload();
+	unHookAll();
+	
 	exit(0);
 }
 
+BOOL CtrlHandler( DWORD fdwCtrlType ) 
+{ 
+  switch( fdwCtrlType ) 
+  { 
+    // Handle the CTRL-C signal. 
+    case CTRL_C_EVENT: 
+	{
+      printf( "Ctrl-C event\n\n" );
+      //Beep( 750, 300 ); 
+		UAP_REQUEST(getModuleInstance(), lb_I_PluginManager, PM)
+		PM->unload();
+		unHookAll();
+	
+		exit(0);
+      return( TRUE );
+	}
+#ifdef bla
+    // CTRL-CLOSE: confirm that the user wants to exit. 
+    case CTRL_CLOSE_EVENT: 
+      Beep( 600, 200 ); 
+      printf( "Ctrl-Close event\n\n" );
+      return( TRUE ); 
+ 
+    // Pass other signals to the next handler. 
+    case CTRL_BREAK_EVENT: 
+      Beep( 900, 200 ); 
+      printf( "Ctrl-Break event\n\n" );
+      return FALSE; 
+ 
+    case CTRL_LOGOFF_EVENT: 
+      Beep( 1000, 200 ); 
+      printf( "Ctrl-Logoff event\n\n" );
+      return FALSE; 
+ 
+    case CTRL_SHUTDOWN_EVENT: 
+      Beep( 750, 500 ); 
+      printf( "Ctrl-Shutdown event\n\n" );
+      return FALSE; 
+#endif
+    default: 
+      return FALSE; 
+  } 
+} 
+ 
+
+
+
 /*...smain:0:*/
 int main(int argc, char** argv) {
-	UAP_REQUEST(getModuleInstance(), lb_I_ApplicationServer, appServer)
-	setLogActivated(true);
-	_CL_LOG << "Global application server is starting..." LOG_
-	setLogActivated(false);
+	{
+		UAP_REQUEST(getModuleInstance(), lb_I_String, s)
+		UAP_REQUEST(getModuleInstance(), lb_I_PluginManager, PM)
+		UAP_REQUEST(getModuleInstance(), lb_I_ApplicationServer, appServer)
+		PM->initialize();
+		setLogActivated(true);
+		_CL_LOG << "Global application server is starting..." LOG_
+		setLogActivated(false);
+	
+		/**
+		* This let the app not crashing. But the handler simply
+		* does exit(0), no cleanup yet ???
+		*/
+#ifndef WINDOWS
+		signal(SIGINT, handler);
+		signal(SIGBREAK, handler);
+		signal(SIGTERM, handler);
+#endif
 
-	/**
-	 * This let the app not crashing. But the handler simply
-	 * does exit(0), no cleanup yet ???
-	 */
-	//signal(SIGINT, handler);
-
-	//signal(SIGILL, handler);
-
- 	if (appServer != NULL) {
-		appServer->run();
-	} else {
-		printf("Global application server not found...\n");
+#ifdef WINDOWS
+		// As of http://msdn.microsoft.com/en-us/library/ms685049%28v=vs.85%29.aspx
+		SetConsoleCtrlHandler( (PHANDLER_ROUTINE) CtrlHandler, TRUE );
+#endif
+	
+		if (appServer != NULL) {
+			appServer->run();
+			PM->unload();
+		} else {
+			printf("Global application server not found...\n");
+		}
+		printf("Global application server is ending...\n");
+		unHookAll();
 	}
-	printf("Global application server is ending...\n");
 	return 0;
 }
 /*...e*/
