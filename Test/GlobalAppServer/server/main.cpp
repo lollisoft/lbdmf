134a02d0b8185b4f03db879a81ef531f4c597b8c
lothar <lothar>
2024-10-14 21:54:14 +0000

    Basically got the Watcom building and client communicating with server

--- Test/GlobalAppServer/server/main.cpp
+++ Test/GlobalAppServer/server/main.cpp
@@ -6,12 +6,12 @@
 
 
 void handler(int sig) {
-	COUT << "Oops..." << ENDL;
-	UAP_REQUEST(getModuleInstance(), lb_I_PluginManager, PM)
-	PM->unload();
-	unHookAll();
-	
-	exit(0);
+        COUT << "Oops..." << ENDL;
+        UAP_REQUEST(getModuleInstance(), lb_I_PluginManager, PM)
+        PM->unload();
+        unHookAll();
+        
+        exit(0);
 }
 
 #ifdef WINDOWS
@@ -21,16 +21,16 @@ bool CtrlHandler( DWORD fdwCtrlType )
   { 
     // Handle the CTRL-C signal. 
     case CTRL_C_EVENT: 
-	{
+        {
       printf( "Ctrl-C event\n\n" );
       //Beep( 750, 300 ); 
-		UAP_REQUEST(getModuleInstance(), lb_I_PluginManager, PM)
-		PM->unload();
-		unHookAll();
-	
-		exit(0);
+                UAP_REQUEST(getModuleInstance(), lb_I_PluginManager, PM)
+                PM->unload();
+                unHookAll();
+        
+                exit(0);
       return( TRUE );
-	}
+        }
 #ifdef bla
     // CTRL-CLOSE: confirm that the user wants to exit. 
     case CTRL_CLOSE_EVENT: 
@@ -63,56 +63,58 @@ bool CtrlHandler( DWORD fdwCtrlType )
 
 
 /*...smain:0:*/
-int main(int argc, char** argv) {
-	{
-		UAP_REQUEST(getModuleInstance(), lb_I_String, s)
-		UAP_REQUEST(getModuleInstance(), lb_I_PluginManager, PM)
-		UAP_REQUEST(getModuleInstance(), lb_I_ApplicationServer, appServer)
+int main(int argc, char** argv)
+{
+        {
+                char* LOGGING = getenv("LOGGING");
 
-		char* LOGGING = getenv("LOGGING");
+                // Default
+                setLogActivated(false);
+                if (LOGGING != NULL) {
+                        if ((strcmp(LOGGING, "no") != 0) && (strcmp(LOGGING, "NO") != 0) && (strcmp(LOGGING, "No") != 0) && (strcmp(LOGGING, "nO") != 0)) 
+                        // Activate
+                                setLogActivated(true);
+                        else
+                                setLogActivated(false);
+                }
 
-		// Default
-		setLogActivated(false);
-		if (LOGGING != NULL) {
-			if ((strcmp(LOGGING, "no") != 0) && (strcmp(LOGGING, "NO") != 0) && (strcmp(LOGGING, "No") != 0) && (strcmp(LOGGING, "nO") != 0)) 
-			// Activate
-				setLogActivated(true);
-			else
-				setLogActivated(false);
-		}
+                if (argc > 1 && strcmp(argv[1], "-log") == 0) {
+                        setLogActivated(true);
+                }
 
-		if (argc > 1 && strcmp(argv[1], "-log") == 0) {
-			setLogActivated(true);
-		}
+                UAP_REQUEST(getModuleInstance(), lb_I_String, s)
+                UAP_REQUEST(getModuleInstance(), lb_I_PluginManager, PM)
+                UAP_REQUEST(getModuleInstance(), lb_I_ApplicationServer, appServer)
 
-		PM->initialize();
-		
-		_CL_LOG << "Global application server is starting..." LOG_
-		
-	
-		/**
-		* This let the app not crashing. But the handler simply
-		* does exit(0), no cleanup yet ???
-		*/
+                PM->initialize();
+                
+                _CL_LOG << "Global application server is starting..." LOG_
+                
+        
+                /**
+                * This let the app not crashing. But the handler simply
+                * does exit(0), no cleanup yet ???
+                */
 #ifndef WINDOWS
-		signal(SIGINT, handler);
-		signal(SIGTERM, handler);
+                signal(SIGINT, handler);
+                signal(SIGTERM, handler);
 #endif
 
 #ifdef WINDOWS
-		// As of http://msdn.microsoft.com/en-us/library/ms685049%28v=vs.85%29.aspx
-		SetConsoleCtrlHandler( (PHANDLER_ROUTINE) CtrlHandler, TRUE );
+                // As of http://msdn.microsoft.com/en-us/library/ms685049%28v=vs.85%29.aspx
+                SetConsoleCtrlHandler( (PHANDLER_ROUTINE) CtrlHandler, TRUE );
 #endif
-	
-		if (appServer != NULL) {
-			appServer->run();
-			PM->unload();
-		} else {
-			printf("Global application server not found...\n");
-		}
-		printf("Global application server is ending...\n");
-		unHookAll();
-	}
-	return 0;
+        
+                if (appServer != NULL) {
+                        appServer->run();
+                        PM->unload();
+                } else {
+                        printf("Global application server not found...\n");
+                }
+                printf("Global application server is ending...\n");
+        }
+
+        unHookAll();
+        return 0;
 }
 /*...e*/
