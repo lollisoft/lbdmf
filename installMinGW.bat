21df5b6792f2b6589892390efaac7ef71ccf6cbb
lothar <lothar>
2025-03-23 10:12:34 +0000

    7z downloaded as 64 bit version fpr 32 bit build. That did not work on 32 bit Windows.

--- installMinGW.bat
+++ installMinGW.bat
@@ -5,8 +5,8 @@ if NOT EXIST 7z2301-x64.exe (
 %Systemroot%\System32\curl -k -L -o 7z2301-x64.exe https://www.7-zip.org/a/7z2301-x64.exe
 )
 
-if NOT EXIST 7z2301-x64.exe (
-curl -k -L -o 7z2301-x64.exe https://www.7-zip.org/a/7z2301-x64.exe
+if NOT EXIST 7z2301-x32.exe (
+curl -k -L -o 7z2301-x32.exe https://www.7-zip.org/a/7z2301.exe
 )
 
 if NOT EXIST MinGW32.7z (
@@ -16,7 +16,7 @@ if NOT EXIST MinGW32.7z (
 curl -k -L -o MinGW32.7z https://sourceforge.net/projects/mingw-w64/files/Toolchains%%20targetting%%20Win32/Personal%%20Builds/mingw-builds/8.1.0/threads-win32/dwarf/i686-8.1.0-release-win32-dwarf-rt_v6-rev0.7z
 )
 
-7z2301-x64.exe /S /D="C:\lbDMF\Develop\Projects\bin"
+7z2301-x32.exe /S /D="C:\lbDMF\Develop\Projects\bin"
 C:\lbDMF\Develop\Projects\bin\7z x MinGW32.7z -oC:\lbDMF\Develop\Tools
 @REM %DEVLW%\lbDMF\Develop\Tools\MinGW\bin\mingw-get install mingw
 @REM %DEVLW%\lbDMF\Develop\Tools\MinGW\bin\mingw-get install g++
