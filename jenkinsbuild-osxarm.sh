47e9b85289212f58dd8011cc2b6317e23359cb3c
lothar <lothar>
2025-04-06 22:36:29 +0000

    Passed prefix into generated makefile as well.

--- jenkinsbuild-osxarm.sh
+++ jenkinsbuild-osxarm.sh
@@ -38,11 +38,11 @@ done
 echo dummy: >> Projects/$REPO_NAME/makefile
 echo \ >> Projects/$REPO_NAME/makefile
 echo BaseDevelopment: dummy >> Projects/$REPO_NAME/makefile
-echo \	\$\(MAKE\) -s -C \$\@ -e LBWXVERSION=current -e DEVROOT=\$\(DEVROOT\) >> Projects/$REPO_NAME/makefile
+echo \	\$\(MAKE\) -s -C \$\@ -e LBWXVERSION=current -e DEVROOT=\$\(DEVROOT\) -e prefix=$(prefix) >> Projects/$REPO_NAME/makefile
 
 echo \ >> Projects/$REPO_NAME/makefile
 echo install: dummy >> Projects/$REPO_NAME/makefile
-echo \	\$\(MAKE\) -s -C BaseDevelopment -e LBWXVERSION=current -e DEVROOT=\$\(DEVROOT\) install >> Projects/$REPO_NAME/makefile
+echo \	\$\(MAKE\) -s -C BaseDevelopment -e LBWXVERSION=current -e DEVROOT=\$\(DEVROOT\) -e prefix=$(prefix) install >> Projects/$REPO_NAME/makefile
 
 DEVROOT=`pwd`
 export DEVROOT
