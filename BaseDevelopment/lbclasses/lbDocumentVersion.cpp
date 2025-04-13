7dd6f95153324800e81abc32081fdd1d34912d9a
lothar <lothar>
2025-04-13 07:16:23 +0000

    Changed my address and copyright start and ending year to reflect my upcoming new home and timeframe of development.

--- BaseDevelopment/lbclasses/lbDocumentVersion.cpp
+++ BaseDevelopment/lbclasses/lbDocumentVersion.cpp
@@ -1,41 +1,45 @@
 /*...sLicence:0:*/
 /*
-	DMF Distributed Multiplatform Framework (the initial goal of this library)
-	This file is part of lbDMF.
-	Copyright (C) 2002-2025 Lothar Behrens (lothar.behrens@lollisoft.de)
+    DMF Distributed Multiplatform Framework (the initial goal of this library)
+    This file is part of lbDMF.
+    Copyright (C) 2000-2025  Lothar Behrens (lothar.behrens@lollisoft.de)
 
-	This library is free software; you can redistribute it and/or
-	modify it under the terms of the GNU Lesser General Public
-	License as published by the Free Software Foundation; either
-	version 2.1 of the License, or (at your option) any later version.
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
 
-	This library is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-	Lesser General Public License for more details.
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
 
-	You should have received a copy of the GNU Lesser General Public
-	License along with this library; if not, write to the Free Software
-	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 
-	The author of this work will be reached by e-Mail or paper mail.
-	e-Mail: lothar.behrens@lollisoft.de
-	p-Mail: Lothar Behrens
-			Ginsterweg 4
+    The author of this work will be reached by e-Mail or paper mail.
+    e-Mail: lothar.behrens@lollisoft.de
+    p-Mail: Lothar Behrens
+            Odenwaldstr. 14
 
-			65760 Eschborn (germany)
+            63150 Heusenstamm (germany)
 */
 /*...e*/
 
 /*...sRevision history:0:*/
 /**************************************************************
 * $Locker:  $
-* $Revision: 1.2.2.4 $
+* $Revision: 1.2.2.5 $
 * $Name:  $
-* $Id: lbDocumentVersion.cpp,v 1.2.2.4 2025/04/05 16:16:30 lothar Exp $
+* $Id: lbDocumentVersion.cpp,v 1.2.2.5 2025/04/13 07:16:23 lothar Exp $
 *
 * $Log: lbDocumentVersion.cpp,v $
+* Revision 1.2.2.5  2025/04/13 07:16:23  lothar
+* Changed my address and copyright start and ending year to reflect
+* my upcoming new home and timeframe of development.
+*
 * Revision 1.2.2.4  2025/04/05 16:16:30  lothar
 * This file seems to clash with HEAD bransh when migrating to git.
 * So update it in the bransh to probably fix this. Why does the
