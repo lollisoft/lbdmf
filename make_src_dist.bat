4b747da9b903316512190d6236ff9b305dd98db8
lothar <lothar>
2025-08-04 17:11:03 +0000

    Added xslt  1.1.34 into the rule for repackaging into source installer

--- make_src_dist.bat
+++ make_src_dist.bat
@@ -104,6 +104,7 @@ call make_dist_base.bat vendor\wxShapeFramework
 call make_dist_base.bat vendor\wxaui-0.9.1
 call make_dist_base.bat vendor\libxml2-2.8.0
 call make_dist_base.bat vendor\libxslt-1.1.22
+call make_dist_base.bat vendor\libxslt-1.1.34
 call make_dist_base.bat vendor\wxJSON-1.2.1
 call make_dist_base.bat include 
 call make_dist_base.bat interfaces 
