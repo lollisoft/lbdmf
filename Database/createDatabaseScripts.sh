3448935b6212e259a3401b6827696608b30e9423
lothar <lothar>
2026-01-27 04:45:11 +0000

    Changed the root directory name

--- Database/createDatabaseScripts.sh
+++ Database/createDatabaseScripts.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd $DEVROOT/Projects/CPP
+cd $DEVROOT/Projects/lbdmf
 
 cat > AppDevelopment/XSLT_Templates/XMIToDMF/XMISettings.xsl << EOF
 <xsl:stylesheet version="1.1" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:exsl="http://exslt.org/common" extension-element-prefixes="exsl">
